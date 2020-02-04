0a33f7c1f2467b371a41305b8662cb9bff2ef324
Sachito-Hirao <sachito.hirao@ibm.com>
2020-02-05 01:16:51 +0900

    AAA

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 
 
 func main() {
-    fmt.Println("Hello")
+http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello World")
+    })
+
+        // ポート = 8686
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal("ListenAndServe failed.", err)
+    }
 }
 
